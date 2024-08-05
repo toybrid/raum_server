966b49e00a28b1bd104b490cb3f1cbdb3ecef4f6
Arjun Thekkumadathil <arjun@Arjuns-MacBook-Air.local>
2024-08-05 12:27:51 +0100

    added allowed host to Env var

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,4 +1,8 @@
+import os
 from helpers.constants import BUILTIN_QUERY_PARAMS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def build_filters(query_parms):
     query_dict = query_parms.dict()
@@ -7,3 +11,7 @@ def build_filters(query_parms):
             query_dict.pop(i)
     return query_dict
 
+def get_allowed_hosts():
+    allowd_hosts = os.getenv('RAUM_ALLOWED_HOSTS', None)
+    domain_list = allowd_hosts.split(",")
+    return domain_list
