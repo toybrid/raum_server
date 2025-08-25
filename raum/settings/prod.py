2d786a83c8c9b74bbad7c0fc7f83e48cb0157d25
Arjun Thekkumadathil <arjun@Arjuns-MacBook-Air.local>
2025-08-25 15:35:14 +0530

    fixed env var names

--- raum/settings/prod.py
+++ raum/settings/prod.py
@@ -8,11 +8,11 @@ load_dotenv()
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql',
-    'NAME': getenv('RAUM_PPROD_DB_NAME'),
-    'USER': getenv('RAUM_PPROD_DB_USER'),
-    'PASSWORD': getenv('RAUM_PPROD_DB_PASSWORD'),
-    'HOST': getenv('RAUM_PPROD_DB_HOST'),
-    'PORT': getenv('RAUM_PPROD_DB_PORT'),
+    'NAME': getenv('RAUM_PROD_DB_NAME'),
+    'USER': getenv('RAUM_PROD_DB_USER'),
+    'PASSWORD': getenv('RAUM_PROD_DB_PASSWORD'),
+    'HOST': getenv('RAUM_PROD_DB_HOST'),
+    'PORT': getenv('RAUM_PROD_DB_PORT'),
     'OPTIONS': {
       'sslmode': 'require',
     },
