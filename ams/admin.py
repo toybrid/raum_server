a1ff355ee61a9bce4624899b4f6e69cd049511c5
Arjun Thekkumadathil <arjun@Arjuns-MacBook-Air.local>
2026-01-03 18:42:48 +0530

    bugfix search and added approval data

--- ams/admin.py
+++ ams/admin.py
@@ -40,21 +40,27 @@ class BundleTypeAdmin(admin.ModelAdmin):
 @admin.register(Container)
 class ContainerAdmin(admin.ModelAdmin):
     list_display = ("id", "code", "container_type", "updated_at", "created_at", "created_by", "updated_by")
-    search_fields = ("code", "label")
+    search_fields = ("code", "container_type__code")
     list_filter = ("container_type", 'project__code')
     ordering = ("code",)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "container", "element", "variant", "component", "layer", "filepath","version","updated_at", "created_at", "created_by", "updated_by")
-    search_fields = ("element", "variant", "component", "layer")
-    # list_filter = ("container__container_type", "status")
+    list_display = (
+        "id", "container", "element", "variant", "component", "layer", "filepath","version", "status", 
+        "approved_at", "approved_by", "updated_at", "created_at", "created_by", "updated_by"
+        )
+    search_fields = ("element__code", "variant", "component", "layer", "filepath")
+    list_filter = ("element", "status")
     ordering = ("element",)
 
 
 @admin.register(Bundle)
 class BundleAdmin(admin.ModelAdmin):
-    list_display = ("id", "container","package","bundle_type", "version", "description", "updated_at", "created_at", "created_by", "updated_by")
+    list_display = (
+        "id", "container","package","bundle_type", "version", "description", 
+        "updated_at", "created_at", "created_by", "updated_by"
+        )
     search_fields = ("bundle_type__code", "bundle_type__label")
     list_filter = ("bundle_type",)
     ordering = ("bundle_type__code",)
